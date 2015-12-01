84a5f2e1f7dc6274eb1a1ba27ec313cbe19e8554
Kevin Sooley <sooleyka@gmail.com>
2015-11-30 22:40:18 -0500

    beautified with bootstrap. bootified?

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -87,4 +87,24 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, she goes back to sleep
+
+    def test_layout_and_styling(self):
+        # Edith visits the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512, 
+            delta = 5
+        )
+
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512, 
+            delta = 5
+        )
 
